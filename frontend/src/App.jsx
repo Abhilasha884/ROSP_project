e7121477e37700cfdf614209fa764e4b127bab23
Abhilasha884 <abhilasha.ishware10@gmail.com>
2025-09-14 12:00:51 +0530

    updated code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
-import Appliances from "./components/Appliances";
-import Cost from "./components/Cost";
+import Appliances from "./components/appliances";
+import Cost from "./components/cost";
 
 export default function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Dashboard />} />   {/* 👈 default route */}
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/appliances" element={<Appliances />} />
         <Route path="/cost" element={<Cost />} />
@@ -15,5 +16,3 @@ export default function App() {
     </Router>
   );
 }
-
-
